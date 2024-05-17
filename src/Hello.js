6b00da747abfc193d872737e171599aec919e0f2
Kim Jihyun <legojhk@gmail.com>
2024-05-18 00:35:30 +0900

    Hello world

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Hello extends React.Component{
+    render(){
+        return(
+            <div className="Hello">
+                <label for="Hello">안녕하세요!</label>
+            </div>
+        );
+    }
+}
+
+export default Hello
