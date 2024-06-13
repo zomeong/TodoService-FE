1536aeb4325c723e5a6c4cbdd3c271215aa103aa
Kim Jihyun <legojhk@gmail.com>
2024-06-13 18:12:52 +0900

    날씨 openAPI 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import TodoList from './TodoList';
 import AddTodo from './AddTodo';
-import { Container, Grid, Button, AppBar, Toolbar, Typography } from "@material-ui/core";
+import OpenApi from './OpenApi';
 import './App.css';
+import { Container, Grid, Button, AppBar, Toolbar, Typography } from "@material-ui/core";
 import { call, signout } from './service/ApiService';
 
 class App extends React.Component {
@@ -33,17 +34,20 @@ class App extends React.Component {
   render() {
     const navigationBar = (
       <AppBar position="static">
-        <Toolbar>
-          <Grid justify="space-between" container>
-            <Grid item>
-              <Typography variant="h6">오늘의 할일</Typography>
-            </Grid>
-            <Grid item>
-              <Button color="inherit" onClick={signout}>logout</Button>
-            </Grid>
-          </Grid>
-        </Toolbar>
-      </AppBar>
+            <Toolbar>
+                <Grid container alignItems="center" justify="space-between">
+                    <Grid item>
+                        <Typography variant="h6">오늘의 할일</Typography>
+                    </Grid>
+                    <Grid item>
+                        <OpenApi />
+                    </Grid>
+                    <Grid item>
+                        <Button color="inherit" onClick={signout}>Logout</Button>
+                    </Grid>
+                </Grid>
+            </Toolbar>
+        </AppBar>
     );
 
     const content = this.state.loading ? <h1>로딩중..</h1> : (
